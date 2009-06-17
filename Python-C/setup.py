dcd1601f7bba9c9bd0ba1080a8165b20b77e90dc
Aymeric Augustin <aymeric.augustin@m4x.org>
2009-06-17 23:46:20 +0200

    Enrich setup.py.

--- Python-C/setup.py
+++ Python-C/setup.py
@@ -4,7 +4,12 @@
 from distutils.core import setup, Extension
 
 setup(
-    name="csudoku",
-    version="1.0",
-    ext_modules=[Extension("csudoku", ["csudoku.c"])]
+    name='csudoku',
+    version='2.0',
+    author='Aymeric Augustin',
+    author_email='aymeric.augustin@m4x.org',
+    description='SuDoKu generator and solver',
+    scripts=['sudoku'],
+    py_modules=['sudoku'],
+    ext_modules=[Extension('csudoku', ['csudoku.c'])],
 )
