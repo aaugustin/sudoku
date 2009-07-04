d6b8ba5d26d8d83a0b9b8b8dcd25c889fb563772
Aymeric Augustin <aymeric.augustin@m4x.org>
2009-07-04 19:02:55 +0200

    Add a script to look for leaks in the C extension.

---
+++ Python-C/test/test_memory.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python
+# Copyright (c) 2008-2009 Aymeric Augustin
+
+"""Memory debugging script for the C implementation."""
+
+from __future__ import with_statement
+
+import os.path, sys
+sys.path[:0] = [os.path.abspath(os.path.join(os.path.dirname(__file__), '..'))]
+from sudoku import SuDoKu
+from guppy import hpy
+
+with open(os.path.join(os.path.dirname(__file__), 'hardest_sudoku.sdk')) as f:
+    problem = f.read()
+
+def run_tests():
+    hpy().heapu() # pre-heating
+    print '\n========   begin    ========\n'
+    print hpy().heapu()
+    print '\n========    init    ========\n'
+    s = SuDoKu()
+    print hpy().heapu()
+    print '\n========    read    ========\n'
+    s.from_string(problem)
+    print hpy().heapu()
+    print '\n========  resolve   ========\n'
+    s.resolve()
+    print hpy().heapu()
+    print '\n========  generate  ========\n'
+    s.generate()
+    print hpy().heapu()
+    print '\n========    end     ========\n'
+
+if __name__ == '__main__':
+    run_tests()
