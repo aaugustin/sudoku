f2f558a958b0eb654ac965f4162e0bc9dc970365
Aymeric Augustin <aymeric.augustin@m4x.org>
2021-08-21 16:53:06 +0200

    Improve display of work in progress.

--- go/sudoku/assets/script.js
+++ go/sudoku/assets/script.js
@@ -1,4 +1,41 @@
 document.addEventListener("DOMContentLoaded", function () {
+    // If a <td contenteditable> element contains nothing, its height
+    // collapses to zero and the cursor isn't vertically centered anymore.
+    // Add a line break, which the browser ignores, to avoid this problem.
+    // Firefox uses this workaround; see also:
+    // https://bugzilla.mozilla.org/show_bug.cgi?id=503838
+
+    function fixEmptyCell(cell) {
+        if (cell.textContent === "") {
+            cell.innerHTML = "<br>";
+        }
+    }
+
+    document.querySelector("table").addEventListener("input", function (event) {
+        fixEmptyCell(event.target);
+    });
+
+    // Add classes to <td contenteditable> elements based on their contents.
+
+    function setCellMarkers(cell) {
+        if (cell.textContent.match(/^[1-9]*$/)) {
+            cell.classList.remove("invalid");
+        } else {
+            cell.classList.add("invalid");
+        }
+        if (cell.textContent.length < 2) {
+            cell.classList.remove("temporary");
+        } else {
+            cell.classList.add("temporary");
+        }
+    }
+
+    document.querySelector("table").addEventListener("input", function (event) {
+        setCellMarkers(event.target);
+    });
+
+    // Save and restore input state with location hash.
+
     const inputs = Array.from(document.querySelectorAll("[contenteditable]"));
     const separator = "|";
 
@@ -13,11 +50,9 @@ document.addEventListener("DOMContentLoaded", function () {
             "Cannot restore grid, was the URL truncated?"
         );
         inputs.forEach(function (input, index) {
-            if (contents[index] === "") {
-                input.innerHTML = "<br>";
-            } else {
-                input.textContent = contents[index];
-            }
+            input.textContent = contents[index];
+            fixEmptyCell(input);
+            setCellMarkers(input);
         });
     }
 
@@ -38,18 +73,4 @@ document.addEventListener("DOMContentLoaded", function () {
     restoreState();
     window.addEventListener("hashchange", restoreState);
     document.querySelector("table").addEventListener("input", saveState);
-
-    // If a <td contenteditable> element contains nothing, its height
-    // collapses to zero and the cursor isn't vertically centered anymore.
-    // Add a line break, which the browser ignores, to avoid this problem.
-    // Firefox uses this workaround; see also:
-    // https://bugzilla.mozilla.org/show_bug.cgi?id=503838
-
-    function fixEmptyCells(event) {
-        if (event.target.textContent === "") {
-            event.target.innerHTML = "<br>";
-        }
-    }
-
-    document.querySelector("table").addEventListener("input", fixEmptyCells);
 });
